19ff6e9f6471964b466e92f925803bba90a9ba4e
GeorgianaMM <georgiana.marti.gg@gmail.com>
2024-09-13 19:20:44 +0300

    Get name and department

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,41 @@
 package emailapp;
 
-public class Email {
+import java.util.Scanner;
 
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String department;
+    private String password;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    //Constructior to get lastname and first name
+    public Email(String fn, String ln) {
+        this.firstName = fn;
+        this.lastName = ln;
+        System.out.println("New email for: " + firstName + " " + lastName);
+
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
+    }
+
+    //Department
+    private String setDepartment(){
+        System.out.println("Department codes:");
+        System.out.println("1 for Sales");
+        System.out.println("2 for Development");
+        System.out.println("3 for Accounting");
+        System.out.println("0 for none");
+        System.out.println("Enter the department code:");
+
+        Scanner scanner = new Scanner(System.in);
+        int depChoice = scanner.nextInt();
+
+        if(depChoice == 1) return "Sales";
+        else if(depChoice == 2) return "Development";
+        else if(depChoice == 3) return "Accounting";
+        else return "";
+    }
+    
 }
